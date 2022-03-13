8e368d4ba674493587defc2719389f2966e2750b
eliazino <elias.akin@gmail.com>
2022-03-13 15:59:50 +0100

    Add project files.

---
+++ HackerRankSolutionRepo/Problems/Warmup1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankSolutionRepo.Problems {
+    public class Warmup1 {
+        public static void plusMinus(List<int> arr) {
+
+        }
+    }
+}
